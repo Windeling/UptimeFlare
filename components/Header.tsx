4c183e865b23f0f844d7ca66fa21186f9313e3b7
Windeling <142467582+windeling@users.noreply.github.com>
2025-03-31 15:53:34 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,10 +21,6 @@ export default function Header() {
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
         <div>
-          <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
-            <Text size="xl" span>
-              🕒
-            </Text>
             <Text
               size="xl"
               span
@@ -32,7 +28,7 @@ export default function Header() {
               variant="gradient"
               gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
             >
-              UptimeFlare
+              Windeling系统单元监测
             </Text>
           </a>
         </div>
