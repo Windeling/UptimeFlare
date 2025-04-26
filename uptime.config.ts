180283d2dd20316a946da04e1307a235c926348c
Windeling <142467582+Windeling@users.noreply.github.com>
2025-04-26 22:20:15 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -11,7 +11,7 @@ const pageConfig = {
   // If not specified, all monitors will be shown in a single list
   // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
   group: {
-    "🌐 公开": ['blog_monitor',"drive_monitor","yf_monitor"],
+    "🌐 公开": ['blog_monitor',"drive_monitor",'analy_monitor'],
     "🔐 私有": ['ccb_monitor'],
   },
 }
@@ -59,41 +59,6 @@ const workerConfig = {
   //    checkLocationWorkerRoute: 'https://xxx.example.com',
     },
 
-    
-    {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'yf_monitor',
-      // `name` is used at status page and callback message
-      name: '驅逐入侵校區計畫',
-      // `method` should be a valid HTTP Method
-      method: 'GET',
-      // `target` is a valid URL
-      target: 'https://stupidyf.pages.dev/',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: '驅逐入侵校區計畫页面监测',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://stupidyf.pages.dev/',
-      // [OPTIONAL] `hideLatencyChart` will hide status page latency chart if set to true
-      hideLatencyChart: false,
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-     // timeout: 10000,
-      //[OPTIONAL] headers to be sent
-    //  headers: {
-     //   'User-Agent': 'Uptimeflare',
-     //   Authorization: 'Bearer YOUR_TOKEN_HERE',
-    //  },
-      // [OPTIONAL] body to be sent
-    //  body: 'text',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-  //    responseKeyword: 'Success',
-      // [OPTIONAL] if specified, the response must NOT contains the keyword to be considered as operational.
-   //   responseForbiddenKeyword: 'Bad gateway',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-  //    checkLocationWorkerRoute: 'https://xxx.example.com',
-    },
 //text
    {
       // `id` should be unique, history will be kept if the `id` remains constant
@@ -164,6 +129,77 @@ const workerConfig = {
       // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
 //      checkLocationWorkerRoute: 'https://xxx.example.com',
     },
+    //text
+   {
+      // `id` should be unique, history will be kept if the `id` remains constant
+      id: 'analy_monitor',
+      // `name` is used at status page and callback message
+      name: 'Windelingの間站点数据分析',
+      // `method` should be a valid HTTP Method
+      method: 'GET',
+      // `target` is a valid URL
+      target: 'https://analytics.windeling.com/',
+      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
+      tooltip: 'Windelingの間访客数据分析',
+      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+      statusPageLink: 'https://analytics.windeling.com/',
+      // [OPTIONAL] `hideLatencyChart` will hide status page latency chart if set to true
+      hideLatencyChart: false,
+      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
+      expectedCodes: [200],
+      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
+    //  timeout: 10000,
+      // [OPTIONAL] headers to be sent
+    //  headers: {
+    //    'User-Agent': 'Uptimeflare',
+   //     Authorization: 'Bearer YOUR_TOKEN_HERE',
+  //    },
+      // [OPTIONAL] body to be sent
+   //   body: 'text',
+      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
+     // responseKeyword: 'Success',
+      // [OPTIONAL] if specified, the response must NOT contains the keyword to be considered as operational.
+   //   responseForbiddenKeyword: 'Bad gateway',
+      // [OPTIONAL] if specified, the check will run in your specified region,
+      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
+//      checkLocationWorkerRoute: 'https://xxx.example.com',
+    },
+
+ {
+      // `id` should be unique, history will be kept if the `id` remains constant
+      id: 'archive_monitor',
+      // `name` is used at status page and callback message
+      name: '网页归档服务',
+      // `method` should be a valid HTTP Method
+      method: 'GET',
+      // `target` is a valid URL
+      target: 'https://archive.windeling.com/',
+      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
+      tooltip: '归纳大大小小各种网站的文章',
+      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+    //  statusPageLink: 'https://analytics.windeling.com/',
+      // [OPTIONAL] `hideLatencyChart` will hide status page latency chart if set to true
+      hideLatencyChart: false,
+      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
+      expectedCodes: [200],
+      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
+    //  timeout: 10000,
+      // [OPTIONAL] headers to be sent
+    //  headers: {
+    //    'User-Agent': 'Uptimeflare',
+   //     Authorization: 'Bearer YOUR_TOKEN_HERE',
+  //    },
+      // [OPTIONAL] body to be sent
+   //   body: 'text',
+      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
+     // responseKeyword: 'Success',
+      // [OPTIONAL] if specified, the response must NOT contains the keyword to be considered as operational.
+   //   responseForbiddenKeyword: 'Bad gateway',
+      // [OPTIONAL] if specified, the check will run in your specified region,
+      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
+//      checkLocationWorkerRoute: 'https://xxx.example.com',
+    },
+
   ],
   notification: {
     // [Optional] apprise API server URL
